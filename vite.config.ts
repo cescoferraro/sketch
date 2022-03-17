368a4c3c16092558c6798c9a82bac616f7ff9c01
Cesco Ferraro <francescoaferraro@gmail.com>
2022-03-16 22:28:50 -0300

    chore: fix deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reactSvgPlugin from "vite-plugin-svg-react";
 
-const reactSvgPlugin = require("vite-plugin-svg-react").default;
 console.log(reactSvgPlugin);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sketch/",
   plugins: [
     reactSvgPlugin({
       useSVGO: true, // optimize svg by SVGO
